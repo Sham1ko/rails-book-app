a1c04ac4f19e6fa3330f287c0e9b42afb62d13d2
Shamshyrak Zholdasbek <shamshyrak.zholdasbek@gmail.com>
2025-05-12 18:33:34 +0200

    t

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,23 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_05_11_161823) do
+  create_schema "auth"
+  create_schema "extensions"
+  create_schema "graphql"
+  create_schema "graphql_public"
+  create_schema "pgbouncer"
+  create_schema "realtime"
+  create_schema "storage"
+  create_schema "vault"
+
   # These are extensions that must be enabled in order to support this database
+  enable_extension "extensions.pg_stat_statements"
+  enable_extension "extensions.pgcrypto"
+  enable_extension "extensions.pgjwt"
+  enable_extension "extensions.uuid-ossp"
+  enable_extension "graphql.pg_graphql"
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "vault.supabase_vault"
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -24,7 +39,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_11_161823) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
-    t.integer "book_id", null: false
+    t.bigint "book_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["book_id"], name: "index_comments_on_book_id"
